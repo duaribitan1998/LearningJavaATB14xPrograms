036b2fcb03b1902c49afb58fbc7fe06734d45bc1
Bitan Duari <work.bitanduari@gmail.com>
2025-09-18 12:56:29 +0530

    Ex02 Of Java Basics ( Comments for Documentation )

---
+++ src/ex_002_JavaBasics/Lab007_Comments.java
@@ -0,0 +1,14 @@
+package ex_002_JavaBasics;
+
+public class Lab007_Comments {
+    /**
+     * Name: Bitan
+     * Leaning Java from TTA
+     * This is an example of comments for Documentation
+     **/
+    public static void main(String[] args) {
+
+        /*usually whatever we write as comment compiler will not detect it*/
+
+        System.out.println(" Documentation Purposes Only");}
+}
