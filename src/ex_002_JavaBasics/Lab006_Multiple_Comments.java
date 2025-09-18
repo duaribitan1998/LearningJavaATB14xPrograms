c89d63d8cf951f795606a9a42f06af1657a4b863
Bitan Duari <work.bitanduari@gmail.com>
2025-09-18 12:22:36 +0530

    Ex02 Of Java Basics (Multiple Comments)

---
+++ src/ex_002_JavaBasics/Lab006_Multiple_Comments.java
@@ -0,0 +1,21 @@
+package ex_002_JavaBasics;
+ /**
+  * Name : Bitan
+  * Passion : Web3 & Blockchain
+  * Learning : JAVA
+  **/
+
+// This is an example of Multiple Comments which is showcases above this //
+
+public class Lab006_Multiple_Comments {
+     /**
+      * Name : Bitan
+      * Passion : Web3 & Blockchain
+      * Learning : JAVA
+      **/
+     // This is an example of Multiple Comments which is showcases above this //
+
+     public static void main(String[] args) {
+         System.out.println(" Multiple Comments Scenario");
+     }
+}
