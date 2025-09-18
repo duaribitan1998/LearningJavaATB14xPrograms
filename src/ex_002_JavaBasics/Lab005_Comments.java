c8513c9a9ed815c712cbcac1297493f0dff9fadd
Bitan Duari <work.bitanduari@gmail.com>
2025-09-18 11:54:32 +0530

    Ex02 Of Java Basics (Comments)

---
+++ src/ex_002_JavaBasics/Lab005_Comments.java
@@ -0,0 +1,13 @@
+package ex_002_JavaBasics;
+
+public class Lab005_Comments {
+    public static void main(String[] args) {
+        // This is also an example of comments
+        // This is also an example of comments
+        // This is also an example of comments
+        // This is also an example of comments
+        // This is also an example of comments
+        // This is also an example of comments
+        System.out.println( "Another Example of Comments");
+    }
+}
