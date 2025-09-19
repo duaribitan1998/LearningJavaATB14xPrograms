0c5f9ee282c51f7d6a569028cac52ee7b20219f2
Bitan Duari <work.bitanduari@gmail.com>
2025-09-19 15:32:08 +0530

    Delete src/Lab018_Boolean.java

--- src/Lab018_Boolean.java
+++
@@ -1,8 +0,0 @@
-public class Lab018_Boolean {
-    public static void main(String[] args) {
-        boolean is_bitan_married = false;
-        boolean does_bitan_invested_in_crypto = true;
-        boolean can_bitan_achieve_his_goal_in_Crypto = true;
-        boolean does_bitan_has_audi = false;
-    }
-}
